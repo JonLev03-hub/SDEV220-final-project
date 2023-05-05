faf05e0ac09e8c668c96bd865d4fd3795e30c82e
Jonathan Leverenz Desktop <jonlev03@gmail.com>
2023-05-05 18:19:08 -0400

    added the customers and suppliers pages with api routes

--- app.py
+++ app.py
@@ -32,7 +32,46 @@ items = [{
         "count": 123,
         "category": "misc_category"
     }]
-
+customers =[
+        {
+        "id": 123,
+        "name": "testItem",
+        "phone":1231231231,
+        "email":"someEmail@test.com"
+    },
+    {
+        "id": 123,
+        "name": "testItem",
+        "phone":1231231231,
+        "email":"someEmail@test.com"
+    },
+    {
+        "id": 123,
+        "name": "testItem",
+        "phone":1231231231,
+        "email":"someEmail@test.com"
+    }
+]
+suppliers =[
+        {
+        "id": 123,
+        "name": "testItem",
+        "phone":1231231231,
+        "email":"someEmail@test.com"
+    },
+    {
+        "id": 123,
+        "name": "testItem",
+        "phone":1231231231,
+        "email":"someEmail@test.com"
+    },
+    {
+        "id": 123,
+        "name": "testItem",
+        "phone":1231231231,
+        "email":"someEmail@test.com"
+    }
+]
 # ----- this route may not be used -------
 # # find a single item 
 # @app.route('/api/item/<int :id>',method = "GET")
@@ -95,6 +134,85 @@ def getItems(category = "all"):
     print(category)
     # return all items with the exact same category. If the category is all return all items. return in a list.
     return jsonify(items)
+
+# add a single item
+@app.route('/api/customer/add', methods=["POST"])
+def addCustomer():
+    data = request.get_json(force=True)
+    print(data)
+    # Add the code to add an customer from the database and if it was successful with a success or fail message along with error code
+    # example body
+    # {
+    #     "id": 123,
+    #     "name": "testItem",
+    #     "phone":1231231231,
+    #     "email":"someEmail@test.com"
+    # }
+    # return {"msg":"failed"}, 400
+    return {"msg":"customer added"}, 200
+
+# return all items
+@app.route('/api/customers/', methods=["GET"])
+@app.route('/api/customers/<string:category>', methods=["GET"])
+def getCustomers(phone = 0):
+    print(phone)
+    # return all customers if phone number = 0, or return the customer that has a matching phone number.
+    # this can be done with phone number, or if its easier I can change it to id, I assumed phone number is more practical
+    return jsonify(customers)
+
+#delete a single item
+@app.route('/api/customer/delete',methods=["DELETE","POST"])
+def deleteCustomer():
+    data = request.get_json(force=True)
+    print(data)
+    # Add the code to remove an customer from the database and if it was successful with a success or fail message along with error code
+    # this can be done with phone number, or if its easier I can change it to id, I assumed phone number is more practical
+    # example body 
+    # {
+    #     "phone": 123,
+    # }
+    # return {"msg":"failed"}, 400
+    return {"msg":"customer removed"}, 200
+
+# add a single item
+@app.route('/api/supplier/add', methods=["POST"])
+def addSupplier():
+    data = request.get_json(force=True)
+    print(data)
+    # Add the code to add an supplier from the database and if it was successful with a success or fail message along with error code
+    # example body
+    # {
+    #     "id": 123,
+    #     "name": "testItem",
+    #     "phone":1231231231,
+    #     "email":"someEmail@test.com"
+    # }
+    # return {"msg":"failed"}, 400
+    return {"msg":"supplier added"}, 200
+
+# return all items
+@app.route('/api/suppliers/', methods=["GET"])
+@app.route('/api/suppliers/<string:category>', methods=["GET"])
+def getSupplier(phone = 0):
+    print(phone)
+    # return all supplier if phone number = 0, or return the supplier that has a matching phone number.
+    # this can be done with phone number, or if its easier I can change it to id, I assumed phone number is more practical
+    return jsonify(suppliers)
+
+#delete a single item
+@app.route('/api/supplier/delete',methods=["DELETE","POST"])
+def deleteSupplier():
+    data = request.get_json(force=True)
+    print(data)
+    # Add the code to remove an supplier from the database and if it was successful with a success or fail message along with error code
+    # this can be done with phone number, or if its easier I can change it to id, I assumed phone number is more practical
+    # example body 
+    # {
+    #     "phone": 123,
+    # }
+    # return {"msg":"failed"}, 400
+    return {"msg":"supplier removed"}, 200
+
 
 if __name__ == "__main__":
     app.run()
---
+++ front end pages/customers/customers.js
@@ -0,0 +1,65 @@
+document
+  .getElementById("addCustomerButton")
+  .addEventListener("click", async (event) => {
+    event.preventDefault();
+    data_items = ["id", "name", "phone", "email"];
+
+    let form = document.getElementById("addCustomerForm");
+    let result = {};
+
+    data_items.forEach((item) => {
+      result[item] = form[item].value;
+    });
+
+    let res = await fetch("http://127.0.0.1:5000/api/customer/add", {
+      method: "POST",
+      body: JSON.stringify(result),
+    });
+    let json = await res.json();
+    window.alert(json.msg);
+    if (res.status == 200) {
+      data_items.forEach((item) => {
+        form[item].value = "";
+      });
+    }
+  });
+
+document
+  .getElementById("searchButton")
+  .addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    let input = document.getElementById("searchInput").value;
+    let res = await fetch(`http://127.0.0.1:5000/api/customers/${input}`, {
+      method: "GET",
+      //   body: JSON.stringify(result),
+    });
+    // console.log(input);
+    let json = await res.json();
+    console.log(json);
+
+    // edit the table data
+    let table = document.getElementById("searchResults");
+    table.innerHTML = "";
+    json.forEach((customer) => {
+      table.innerHTML += `<tr><td>${customer.id}</td><td>${customer.name}</td><td>${customer.phone}</td><td>${customer.email}</td></tr>`;
+    });
+  });
+
+document
+  .getElementById("deleteCustomer")
+  .addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    let input = document.getElementById("deleteInput").value;
+    let result = {
+      phone: input,
+    };
+    let res = await fetch(`http://127.0.0.1:5000/api/customer/delete`, {
+      method: "DELETE",
+      body: JSON.stringify(result),
+    });
+    let json = await res.json();
+    console.log(json);
+    window.alert(json.msg);
+  });
